8d961915b3cf8eaf134968825e31dee301c77d8f
Steven Williams <Steven30135@gmail.com>
2015-02-24 19:34:50 -0800

    Added even more cors

--- api/api/mailchimp/index.js
+++ api/api/mailchimp/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var API = require('./mailchimpHandler.js');
 var mongoose = require('mongoose');
+var request = require('request');
 
 router.post('/mailchimpcall', API.mailchimp);
 
