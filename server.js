15c87dbebd2e022a2e725d5190b364e9b8402fd1
Steven Williams <Steven30135@gmail.com>
2015-02-24 19:32:42 -0800

    Added more cors

--- server.js
+++ server.js
@@ -7,13 +7,14 @@ var express = require('express');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 var app = express();
-
+var cors = require('cors');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
+app.use(cors());
 
 mongoose.connect($config.mongo.url);
-app.use('/api/v1', /*decode(),*/ require('./api'));
+app.use('/api/v1', cors(), /*decode(),*/ require('./api'));
 
 
 app.listen($config.port, function(){
