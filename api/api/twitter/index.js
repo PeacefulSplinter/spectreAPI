20261805c6bfd2d0b5664910bdab5c44a1ea3a5c
Arun Suresh <arunvsuresh@gmail.com>
2015-02-25 19:57:31 -0800

    Add twitterRetweet method to allow user to see his retweets

--- api/api/twitter/index.js
+++ api/api/twitter/index.js
@@ -3,9 +3,9 @@ var router = express.Router();
 var API = require('./twitterController.js');
 var mongoose = require('mongoose');
 
-//router.get('/twitterFollowers', API.twitterFollowers);
+router.get('/twitterFollowers', API.twitterFollowers);
 router.get('/twitterMentions', API.twitterMentions);
-// router.get('/twitterRetweets', API.twitterRetweets);
+router.get('/twitterRetweets', API.twitterRetweets);
 
 module.exports = router;
 
--- api/api/twitter/twitterController.js
+++ api/api/twitter/twitterController.js
@@ -2,46 +2,46 @@
 var Twitter = require('twitter');
 
 
-// exports.twitterFollowers = function (req, res) {
+exports.twitterFollowers = function (req, res) {
 
-// 	// once decoded, find 'where token id (name) matches our database name'
-// 		// check to see if this person has a web token.
-// 		// if so, make the call with it and return data
-// 	var client = new Twitter({
-//     consumer_key: $config.Twitter.API_KEY,
-//     consumer_secret: $config.Twitter.API_SECRET,
-// 	  access_token_key: $config.Twitter.ACCESS_TOKEN_KEY,
-// 	  access_token_secret: $config.Twitter.ACCESS_TOKEN_SECRET
+	// once decoded, find 'where token id (name) matches our database name'
+		// check to see if this person has a web token.
+		// if so, make the call with it and return data
+	var client = new Twitter({
+    consumer_key: $config.Twitter.API_KEY,
+    consumer_secret: $config.Twitter.API_SECRET,
+	  access_token_key: $config.Twitter.ACCESS_TOKEN_KEY,
+	  access_token_secret: $config.Twitter.ACCESS_TOKEN_SECRET
 
-// 	});
+	});
 	
-// 	var data = {
-// 		screen_name: 'arunvs21',
-// 		followersCount: 'followersCount',
-// 		description: 'description'
-// 	}
+	var data = {
+		screen_name: 'arunvs21',
+		followersCount: 'followersCount',
+		description: 'description'
+	}
 
-//   //var params = {screen_name: 'arunvs21'};
-//   // statuses/user_timeline
-//   // friends/list
-//   // followers/list
-// 	client.get('followers/list', data, function(error, followers, response){
-// 	  //if (!error) {
-//       var username = {
-//         'follow': followers['users'].length,
-//         'followDate': [],
-//         'locations': []
-//       }
-// 	  	var users = followers['users'];
-// 	  	for (var i = 0; i < users.length; i++) {
-//         username.followDate.push(users[i]['created_at']);
-//         username.locations.push(users[i]['location']);
-// 	  	}
-//       res.send(username);
-//       console.log(followers);
-// 	});
+  //var params = {screen_name: 'arunvs21'};
+  // statuses/user_timeline
+  // friends/list
+  // followers/list
+	client.get('followers/list', data, function(error, followers, response){
+	  //if (!error) {
+      var username = {
+        'follow': followers['users'].length,
+        'followDate': [],
+        'locations': []
+      }
+	  	var users = followers['users'];
+	  	for (var i = 0; i < users.length; i++) {
+        username.followDate.push(users[i]['created_at']);
+        username.locations.push(users[i]['location']);
+	  	}
+      res.send(username);
+      console.log(followers);
+	});
   
-// };
+};
 
 exports.twitterMentions = function (req, res) {
 	// once decoded, find 'where token id (name) matches our database name'
@@ -75,36 +75,40 @@ exports.twitterMentions = function (req, res) {
       }
 
 	  	res.send(twitterMentions);
-      
+
 	  }
 	});
 
 };
 
-// exports.twitterRetweets = function (req, res) {
-// 	// once decoded, find 'where token id (name) matches our database name'
-// 		// check to see if this person has a web token.
-// 		// if so, make the call with it and return data
-// 	var client = new Twitter({
-// 	  // API_KEY: $config.twitter.API_KEY,
-//    	  // API_SECRET: $config.facebook.API_SECRET,
-// 	  consumer_key: 'bL9j8Lj2NCh13knSterEaS9lI',
-// 	  consumer_secret: 'mHKrS9WYYEBeF7gNA5wT6HzfsO2HGwp1K2AltiecFBB6OE0ySI',
-// 	  access_token_key: '590714963-ge2SCxUguz04xAQtAAwZM4H8IsXCSczVo16ifeVj',
-// 	  access_token_secret: 'IfAnMqBEhHgRERiUTadbacjyzoTBPjxAGkIGfz3WnRZje'
+exports.twitterRetweets = function (req, res) {
+	// once decoded, find 'where token id (name) matches our database name'
+		// check to see if this person has a web token.
+		// if so, make the call with it and return data
+	var client = new Twitter({
 
-// 	});
+	  consumer_key: $config.Twitter.API_KEY,
+    consumer_secret: $config.Twitter.API_SECRET,
+    access_token_key: $config.Twitter.ACCESS_TOKEN_KEY,
+    access_token_secret: $config.Twitter.ACCESS_TOKEN_SECRET
+
+	});
 	 
-// 	var params = {screen_name: 'arunvs21'};
-// 	// statuses/user_timeline
-// 	// friends/list
-// 	// followers/list
-// 	client.get('statuses/retweets_of_me', params, function(error, retweets, response){
-// 	  if (!error) {
-// 	  	// console.log(tweets);
-// 	  	// res.send(tweets);
-// 	    //console.log(tweets);
-// 	  }
-// 	});
+	var params = {screen_name: 'arunvs21'};
 
-// };
+	client.get('statuses/retweets_of_me', params, function(error, retweets, response){
+      var twitterRetweets = {
+        tweet: [],
+        created_at: []
+      }
+
+      for (var i = 0; i < retweets.length; i++) {
+        twitterRetweets.tweet.push(retweets[i]['text']);
+        twitterRetweets.created_at.push(retweets[i]['created_at']);
+      }
+
+      res.send(twitterRetweets);
+
+	});
+
+};
